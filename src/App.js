a60f925cd4d5f795e4be9b7f80aa17260af449d6
git-mhasan <mahadi2020dev@gmail.com>
2022-04-17 15:38:45 +0600

    header with ham menu created.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Header from './components/Shared/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Shared/Footer/Footer';
 import Home from './components/Pages/Home/Home';
+import Checkout from './components/Pages/Checkout/Checkout';
+import Blogs from './components/Pages/Blogs/Blogs';
+import About from './components/Pages/About/About';
+import Login from './components/Pages/Login/Login';
+import NotFound from './components/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,6 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/checkout" element={<Checkout></Checkout>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import logo from '../../../logo.svg';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import "./Header.css"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header Section</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+
+                    <img
+                        src={logo}
+                        alt=""
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                    Construction Consultant
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/checkout">Checkout</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About Me</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
     );
 };
 
